e86ccfb7faa96a6a71e793a36005b78b5aec644c
Victor Alexandre Ferreira Santos <125712570+Vic7ror@users.noreply.github.com>
2025-11-08 04:36:00 -0300

    Atividade Fila em C#

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Fila-Console
+# ATIVIDADE: RESTAURANTE - FILA(QUEUE)
 
+Escreva um programa em linguagem Java/C# que simule o
+funcionamento deste sistema.
+
+Deverá ser exibido um menu com as seguintes opções:
+- Inserção de cliente na fila de pedidos
+- Remoção de cliente da fila de pedidos
+- Remoção de cliente da fila de pagamentos
+- Remoção de cliente da fila de encomendas
+- Sair
+------------------------------------------------------------------------------------------
+- Cada cliente novo que entra na fila de pedidos recebe um código, que vai aumentando
+sequencialmente.
+- Sempre que um cliente entrar em uma fila, deverá ser impressa uma mensagem,
+como no seguinte exemplo: “Cliente 9999 entrou na fila de pedidos.”
+- Sempre que um cliente for removido de um fila, deverá ser impressa uma mensagem,
+como no seguinte exemplo: “Cliente 9999 foi removido da fila de pagamentos”.
+
+------------------------------------------------------------------------------------------
+Lenguage: C#
---
+++ Restaurante(fila)/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante_fila_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+            int COD_cliente = 1;
+            int cliente_anterior = 0;
+            Fila pedidos = new Fila(100);
+            Fila pagamentos = new Fila(100);
+            Fila encomendas = new Fila(100);
+
+            Console.WriteLine("1 - Inserção de cliente na fila de pedidos");
+            Console.WriteLine("2 - Remoção de cliente da fila de pedidos");
+            Console.WriteLine("3 - Remoção de cliente da fila de pagamentos");
+            Console.WriteLine("4 - Remoção de cliente da fila de encomendas");
+            Console.WriteLine("5 - Sair");
+
+            while (opcao != 5)
+            {
+                Console.WriteLine();
+                opcao = int.Parse(Console.ReadLine());
+                if (opcao == 1)
+                {
+                    if (pedidos.Cheia() == true)
+                    {
+                        Console.WriteLine("Fila de pedidos cheia!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        cliente_anterior = COD_cliente;
+                        pedidos.Enfileirar(COD_cliente);
+                        Console.WriteLine("O cliente " + COD_cliente + " foi adicionado à fila de pedidos");
+                        Console.WriteLine();
+                        COD_cliente++;
+                    }
+                }
+                else if (opcao == 2)
+                {
+                    if (pedidos.Vazia() == true)
+                    {
+                            Console.WriteLine("Fila de pedidos Vazia!!");
+                    }
+                    else
+                    {
+                        if (pagamentos.Cheia() == true)
+                        {
+                            Console.WriteLine("Fila de pagamentos cheia!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            cliente_anterior = pedidos.Desenfileirar();
+                            pagamentos.Enfileirar(cliente_anterior);
+                            Console.WriteLine("O cliente " + cliente_anterior + " foi removido da fila de pedidos e adicionado à fila de pagamentos.");
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                else if (opcao == 3)
+                {
+                    if (pagamentos.Vazia() == true)
+                    {
+                        Console.WriteLine("Fila de pagamentos Vazia!!");
+                    }
+                    else
+                    {
+                        if (encomendas.Cheia() == true)
+                        {
+                            Console.WriteLine("Fila de encomendas cheia!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            cliente_anterior = pagamentos.Desenfileirar();
+                            encomendas.Enfileirar(cliente_anterior);
+                            Console.WriteLine("O cliente " + cliente_anterior + " foi removido da fila de pagamentos e adicionado à fila de encomendas.");
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                else if (opcao == 4)
+                {
+                    if (encomendas.Vazia() == true)
+                    {
+                        Console.WriteLine("Fila de encomendas Vazia!!");
+                    }
+                    else
+                    {
+                        cliente_anterior = encomendas.Desenfileirar();
+                        Console.WriteLine();
+                        Console.WriteLine("O cliente " + cliente_anterior + " foi removido da fila de encomendas.");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
